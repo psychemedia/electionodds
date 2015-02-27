def83549e2b51fbaf97492654a9a60a10306002e
Tony Hirst <tony.hirst@gmail.com>
2015-02-27 12:09:30 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,77 @@
 # This is a template for a Python scraper on Morph (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
+constituencyslugs=['isle-of-wight']
+
+import scraperwiki
+
+from __future__ import division
+
+import requests
+import datetime
+from bs4 import BeautifulSoup
+
+def urlbuilder_constituency(constslug):
+  return 'http://www.oddschecker.com/politics/british-politics/{0}/winning-party'.format(constslug)
+
+def makeSoup(url):
+	try:
+		r = requests.get(url)
+		#print '>>>',r.history
+		ret= BeautifulSoup(r.text)
+		for s in r.history:
+			if s.status_code==302: ret==""
+	except: ret=""
+	return ret
+
+def oddsGrabber(soup,default):
+  if soup=="": return {}
+  #soup=makeSoup(url)
+  table=soup.find( "tbody", {"id":"t1"} )
+  allbets=default
+  allbets['time']=datetime.datetime.utcnow()
+  bets={}
+  for row in table.findAll('tr'):
+    name=row('td')[1].string
+    tds = row('td')[3:]
+    bets[name]={}
+    for td in tds:
+      if td.string!=None:
+        try:
+          bets[name][ td['id'].split('_')[1] ]=td.string
+        except: pass
+  allbets['odds']=bets
+  return allbets
+
+def oddsGrabber_constituency(constslug):
+  url=urlbuilder_constituency(constslug)
+  soup=makeSoup(url)
+  if soup=='':
+    return {}
+  return oddsGrabber(soup,{'typ':'constituencyOdds','const':constslug})
+
+def oddsParser(odds):
+  bigodds=[]
+  oddsdata=odds['odds']
+  for party in oddsdata:
+    #data in tidy format
+    data={'time':odds['time'],'const':odds['const']}
+    for bookie in oddsdata[party]:
+      data['party']=party
+      data['bookie']=bookie
+      data['oddsraw']=oddsdata[party][bookie]
+      data['odds']=eval(data['oddsraw'])
+    bigodds.append(data) 
+  return bigodds
+
+
+odds=oddsGrabber_constituency(const)
+
+for const in constituencyslugs:
+  odds=oddsGrabber_constituency(const)
+  oddsdata=oddsParser(odds)
+  scraperwiki.sqlite.save(unique_keys=[], data=oddsdata,table='UKconst2015')
+
 # import lxml.html
 #
 # # Read in a page
